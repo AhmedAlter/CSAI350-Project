2d32c89bdaff0c5fc3be7bd33a0677b41420c051
AhmedAlter <135684426+AhmedAlter@users.noreply.github.com>
2023-11-25 17:08:05 +0400

    Add files via upload

--- typorec.py
+++ typorec.py
@@ -2,9 +2,7 @@ from spellchecker import SpellChecker
 import language_tool_python
 import re
 from sympy import sympify
-import qrcode
-from PIL import Image
-from io import BytesIO 
+
 
 def texto(input_text):
     # Convert input_text to string
@@ -56,25 +54,3 @@ def evaluate_math_operation(input_text):
         return str(result)
     else:
         return None
-def generate_qr_code(url):
-    # Create a QR code instance
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    
-    # Add data to the QR code
-    qr.add_data(url)
-    qr.make(fit=True)
-
-    # Create an image from the QR code instance
-    img = qr.make_image(fill_color="black", back_color="white")
-
-    # Convert PIL Image to bytes-like object
-    img_bytes = BytesIO()
-    img.save(img_bytes, format='PNG')
-    img_bytes = img_bytes.getvalue()
-
-    return img_bytes
